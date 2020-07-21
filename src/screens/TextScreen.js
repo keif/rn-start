e67e9db758a1e297139152b947129f80540d25ce
Keith T. Baker <hello@keif.name>
2020-07-21 16:45:18 -0400

    Handling text input

---
+++ src/screens/TextScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, StyleSheet, View, Button } from "react-native";
+
+const TextScreen = () => {
+    return (
+        <Text>TextScreen</Text>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default TextScreen
