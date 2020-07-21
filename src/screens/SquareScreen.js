7a00ff2b7715bcfc35d64c6ba36cda3d7ab381ec
Keith T. Baker <hello@keif.name>
2020-07-21 15:30:47 -0400

    validating state changes

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,18 +2,31 @@ import React, { useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import ColorCounter from '../components/ColorCounter';
 
-const COLOR_INCREMENT = 10
+const COLOR_INCREMENT = 15
 
 const SquareScreen = (props) => {
     const [red, setRed] = useState(0)
     const [green, setGreen] = useState(0)
     const [blue, setBlue] = useState(0)
+
+    const setColor = (color, change) => {
+        // color === 'red' || 'green' || 'blue'
+        // change === +COLOR_INCREMENT || -COLOR_INCREMENT
+        if (color === `red`) {
+            if (red + change > 255 || red + change < 0) {
+                return
+            } else {
+                setRed(red + change)
+            }
+        }
+    }
+
     return (
         <View>
             <ColorCounter
                 color="Red"
-                onDecrease={() => setRed(red - COLOR_INCREMENT)}
-                onIncrease={() => setRed(red + COLOR_INCREMENT)}
+                onDecrease={() => setColor(`red`, -1 * COLOR_INCREMENT)}
+                onIncrease={() => setColor(`red`, COLOR_INCREMENT)}
             />
             <ColorCounter
                 color="Green"
