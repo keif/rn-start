73c90b3d0b15bd682ddc3c0da759eb9ab0b39d08
Keith T. Baker <hello@keif.name>
2020-07-21 16:42:53 -0400

    convert CounterScreen to useReducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,24 +1,40 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { Text, StyleSheet, View, Button } from "react-native";
 
+const COUNT_INCREMENT = 1
+const COUNT_OBJECT = { count: 0 }
+
+const reducer = (state, action) => {
+    // state === { count: number };
+    // action === { type: 'increase_count' || 'decrease_count', payload: +COUNT_INCREMENT || -COLOR_INCREMENT }
+    switch (action.type) {
+        case `increment`:
+            return { ...state, count: state.count + action.payload }
+        case `decrement`:
+            return { ...state, count: state.count - action.payload }
+        default:
+            return state
+    }
+}
+
 const CounterScreen = (props) => {
-    const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, COUNT_OBJECT)
 
     return (
         <View>
             <Button
                 onPress={() => {
-                    setCounter(counter + 1);
+                    dispatch({ type: `increment`, payload: COUNT_INCREMENT});
                 }}
                 title="Increase"
             />
             <Button
                 onPress={() => {
-                    setCounter(counter - 1);
+                    dispatch({ type: `decrement`, payload: COUNT_INCREMENT});
                 }}
                 title="Decrease"
             />
-            <Text style={styles.header}>Current Count: {counter}</Text>
+            <Text style={styles.header}>Current Count: {state.count}</Text>
         </View>
     );
 
@@ -28,9 +44,6 @@ const styles = StyleSheet.create({
     header: {
         fontSize: 30
     },
-    text: {
-        fontSize: 20
-    }
 });
 
 export default CounterScreen;
